486c2da9ae0e93dda80c1ae3708b0d954a5ffac9
Mateus Franco <mateusfrancofs3@gmail.com>
2019-10-26 17:46:03 -0300

    menu adicionado

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
-import FormGeneric from './FormGeneric'
+
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 class Menu extends Component{
     state = {
@@ -9,19 +12,41 @@ class Menu extends Component{
 
     render(){
         return (
-            <div>
-                <div class="card shadow-sm p-3 mb-5 bg-white rounded">
-                    <div class="card-body">
-                        <div className="form-check">
-                            <FormGeneric  type="radio" typeclass="form-check-input" typeclasslabel="form-check-label" name="FUP"></FormGeneric>
-                        </div>
-                        <div className="form-check">
-                            <FormGeneric  type="radio" typeclass="form-check-input" typeclasslabel="form-check-label" name="Etica"></FormGeneric>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
+            <SideNav onSelect={(selected) => {
+        // Add your code here 
+            }}>
+            <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                    <NavItem eventKey="home">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Home
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="charts">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Charts
+                        </NavText>
+                        <NavItem eventKey="charts/linechart">
+                            <NavText>
+                                Line Chart
+                            </NavText>
+                        </NavItem>
+                        <NavItem eventKey="charts/barchart">
+                            <NavText>
+                                Bar Chart
+                            </NavText>
+                        </NavItem>
+                    </NavItem>
+                </SideNav.Nav>
+            </SideNav>
+
+
         )
     }
 }
